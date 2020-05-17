3ea3056800dc3fb838734d0aaaffb9f44a6feece
fmaulana240699 <57279422+fmaulana240699@users.noreply.github.com>
2020-05-17 16:34:47 +0700

    add file

---
+++ server.py
@@ -0,0 +1,18 @@
+#socket server
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((socket.gethostname(), 2499))
+server.listen(5)
+# socket.AF_INET > untuk IPv4
+# socket.SOCK_STREAM menggunakan protocol tcp
+# server.bind untuk binding on hostname/ip:port
+# server.listen(5) connection/5 user
+#
+
+
+while True:
+	clientsocket, address = server.accept()
+	print(f"connection from {address} has been established")
+	clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+	
