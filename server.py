9dde41faca5168d38ed012a354df59191a8d872e
fmaulana240699 <57279422+fmaulana240699@users.noreply.github.com>
2020-05-17 17:03:24 +0700

    tambah localhost

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 #socket server
 import socket
 
+HOST='localhost'
+PORT=2406
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((socket.gethostname(), 2499))
+server.bind((HOST PORT))
 server.listen(5)
 # socket.AF_INET > untuk IPv4
 # socket.SOCK_STREAM menggunakan protocol tcp
